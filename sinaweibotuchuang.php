c452a0fbc1bca3a5c6f543ee5edbbbcb6e51297e
Fan <theFRP@gmail.com>
2014-09-12 10:04:10 +0800

    修改email

--- sinaweibotuchuang.php
+++ sinaweibotuchuang.php
@@ -9,7 +9,7 @@ Author: Fan
 Author URI: thefrp.sinaapp.com
 */
 /*
-    Copyright 2014  Fan  (email : thefro@gmail.com)
+    Copyright 2014  Fan  (email : thefrp@gmail.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
